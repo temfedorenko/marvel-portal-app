856b41bf9dd2c6cedeb8994a79874fca981eb74c
ArtFED <temfedorenko@gmail.com>
2022-10-31 07:28:41 +0200

    add loading and error messages

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import spinner from './spinner.gif';
+import './spinner.scss';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <img src={spinner} alt="spinner" />
+    </div>
+  );
+};
+
+export default Spinner;
